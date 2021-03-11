892359dc0d225b04328fb3de19d960a80a6f6de8
aditi kumari <kumariaditi017@gmail.com>
2021-03-12 00:14:16 +0530

    Fifth commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const{ check, validationResult} = require('express-validator');
 const User = require('../../models/User');
 //@route POST api/users
@@ -21,13 +25,35 @@ const {name, email, password } = req.body;
 try {
     let user = await User.findOne({email});
     if(user){
-        res.status(400).json({errors:[{msg:"User already exists"}]});
+       return res.status(400).json({errors:[{msg:"User already exists"}]});
     }
+    const avatar = gravatar.url(email,{
+        s:'200',
+        r:'pg',
+        d:'mm'
+    })
+    user = new User({
+        name,
+        email,
+        avatar,
+        password
+    });
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password,salt);
+    await user.save();
+    const payload = {
+        user: {
+            id: user.id
+        }
+    }
+    jwt.sign(payload, config.get('jwtSecret'), {expiresIn:3600}, (err, token)=>{
+        if(err)throw err;
+        res.json({token});
+    });
 // see if user exists
 // get user's gravatar
 // encrypt password
 //return jsonwebtoken
-res.send('User route');
 }catch(err){
     console.error(err.message);
     res.status(500).send('Server error');
