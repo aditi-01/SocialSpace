5e09fb0fbb96090306b1afe54a3959cccba4e9d7
aditi kumari <kumariaditi017@gmail.com>
2021-03-11 15:49:32 +0530

    Second commit

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+//connect Database
+connectDB();
 app.get('/',(req,res) => res.send('API running'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('server started on port ${PORT}'));
